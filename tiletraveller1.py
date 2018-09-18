a6e38565d20a008fe48b13ef61a183e97a446688
Baldur Haraldsson <baldurhar18@ru.is>
2018-09-18 13:47:11 +0000

    fyrstahreyfing

--- tiletraveller1.py
+++ tiletraveller1.py
@@ -1,5 +1,7 @@
 #algrim
 #gera upphafsstaðsetningu
+locationx=1
+locationy=1
 #skilgreina hvernig hann hreyfir sig í gegnum kassana með upp,niður og hliðar tökkum
 #nota while lykkju 
 #búa til hvernig hver kassi fyrir sig er(hvernig er hægt að hreyfa sig í honum The player enters:
@@ -8,4 +10,12 @@
 #• s/S for south (down)
 #• w/W for west (left)
 # prenta út victory ef hann lendir í lokakassanum 
-while 
+while locationx !=3 or locationy !=1:
+    if locationx == 1 and locationy==1 or locationx==2 and locationy==1:
+        print("You can travel: (N)orth.")
+        locationchange = (input("Direction: "))
+        if locationchange.lower() == "n":
+            locationy += 1
+        else:
+            print("Not a valid direction!")
+    
