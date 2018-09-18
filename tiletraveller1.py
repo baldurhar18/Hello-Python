175aaa942a74f5d7914ff0a9944b8c63309e865c
Baldur Haraldsson <baldurhar18@ru.is>
2018-09-18 13:16:52 +0000

    while

---
+++ tiletraveller1.py
@@ -0,0 +1,11 @@
+#algrim
+#gera upphafsstaðsetningu
+#skilgreina hvernig hann hreyfir sig í gegnum kassana með upp,niður og hliðar tökkum
+#nota while lykkju 
+#búa til hvernig hver kassi fyrir sig er(hvernig er hægt að hreyfa sig í honum The player enters:
+#• n/N for north (up)
+#• e/E for east (right)
+#• s/S for south (down)
+#• w/W for west (left)
+# prenta út victory ef hann lendir í lokakassanum 
+while 
