75b4b58009e60fef0354cf1c1565fc101a68b88e
Baldur Haraldsson <baldurhar18@ru.is>
2018-09-18 15:09:23 +0000

    virkar

--- tiletraveller1.py
+++ tiletraveller1.py
@@ -17,24 +17,77 @@ while location != [3,1]:
         print("You can travel: (N)orth.")
         while True:
             locationchange = input("Direction: ")
-            if locationchange == "n":
+            if locationchange.lower() == "n":
                 locationy += 1
                 break
             else:
                 print("Not a valid direction!")
     
-    elif location == [1,2]
+    elif location == [1,2]:
         print("You can travel: (N)orth or (E)ast or (S)outh.")
-        while True
+        while True:
             locationchange = (input("Direction: "))
             if locationchange.lower() == "n":
                 locationy += 1
+                break
             elif locationchange.lower() == "e":
                 locationx +=1
+                break
             elif locationchange.lower() == "s":
                 locationx -= 1
+                break
             else:
                 print("Not a valid direction!")
+    elif location == [1,3]:
+        print("You can travel: (E)ast or (S)outh.")
+        while True:
+            locationchange = (input("Direction: "))
+            if locationchange.lower() == "e":
+                locationx +=1
+                break
+            elif locationchange.lower() == "s":
+                locationx -= 1
+                break
+            else:
+                print("Not a valid direction!")
+    elif location == [2,3]:
+        print("You can travel: (E)ast or (W)est.")
+        while True:
+            locationchange = (input("Direction: "))
+            if locationchange.lower() == "e":
+                locationx +=1
+                break
+            elif locationchange.lower() == "w":
+                locationx -= 1
+                break
+            else:
+                print("Not a valid direction!")
+    elif location == [3,3] or location ==[2,2]:
+        print("You can travel: (S)outh or (W)est.")
+        while True:
+            locationchange = (input("Direction: "))
+            if locationchange.lower() == "s":
+                locationy -=1
+                break
+            elif locationchange.lower() == "w":
+                locationx -= 1
+                break
+            else:
+                print("Not a valid direction!")
+    elif location == [3,2]:
+        print("You can travel: (N)orth or (S)outh.")
+        while True:
+            locationchange = (input("Direction: "))
+            if locationchange.lower() == "n":
+                locationy +=1
+                break
+            elif locationchange.lower() == "s":
+                locationy -= 1
+                break
+            else:
+                print("Not a valid direction!")
+print("Victory!")
+
     
     
     
